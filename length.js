fbe00d48584e4d1d69e6d85ca273040f1476f4fb
shitime <1257201113@qq.com>
2017-09-14 14:06:51 +0800

    验证长度对数组的影响

---
+++ length.js
@@ -0,0 +1,14 @@
+/**
+ * 2017-9-14    Shi Ruitao
+ */
+var arr = [1, 2, 3];
+console.log(arr.length);
+
+arr.length = 6;
+console.log(arr);
+
+arr.length = 2;
+console.log(arr);
+
+arr.length = 3;
+console.log(arr);
