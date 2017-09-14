3a03a1c485a2392543de84d1eeeb38687677a4bf
shitime <1257201113@qq.com>
2017-09-14 20:32:52 +0800

    斐波那契数列

--- for.js
+++ for.js
@@ -14,14 +14,14 @@ var a=0;
  var A = [1, 1];
 for( var i = 2; i <= 100; i++){
     A[i] = A[i - 1] + A[i - 2] ;
-   
-    console.log(A[i]);
 }
+
+console.log(A);
 
 var b = [1, 1];
 
 for(var n = 2; n <=50; n++ ){
     b[n] = b[n - 1] + 2 * b[n - 2];
-
-    console.log(b[n]);
 }
+
+console.log(b);
