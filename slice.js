ef8d9b41903ed78876a962db192b0ae676160c0c
shitime <1257201113@qq.com>
2017-09-14 15:03:28 +0800

    截取数组部分元素组成新数组

---
+++ slice.js
@@ -0,0 +1,27 @@
+/**
+ * 2017-9-14    Shi Ruitao
+ */
+
+var arr = ['a', 'b', 'c', 'd', 'f', 'g'];
+
+console.log( `arr.slice(0,4) => [ ${ arr.slice(0,4) } ]`);
+
+console.log( `arr.slice(2) => [ ${ arr.slice(2) } ]` );
+
+console.log( `arr.slice(-1,1) => [${ arr.slice(-1,1) }]` );
+
+console.log( `arr.slice(0,10) => [ ${ arr.slice(0,10) } ]` );
+
+console.log( `arr.slice(-1,7) => [ ${ arr.slice(-1,7) } ]` );
+
+console.log( `arr.slice(-1,6) => [ ${ arr.slice(-1,6) } ]` );
+
+console.log( `arr.slice(-3,-1) => [ ${ arr.slice(-3,-1) } ]` );
+
+console.log( `arr.slice(-1,9) => [ ${ arr.slice(-1,9) } ]` );
+
+console.log( `arr.slice(-4,2) => [ ${ arr.slice(-4,2) } ]` );
+
+console.log( `arr.slice(-4,3) =>[ ${ arr.slice(-4,3) } ]` );
+
+console.log( arr.length );
