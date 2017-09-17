178a78753a364ff0e8456fa03adb15f4b08e6044
shitime <1257201113@qq.com>
2017-09-17 14:10:43 +0800

    age+1递增--23

--- ageclosure.js
+++ ageclosure.js
@@ -2,19 +2,15 @@
  * 2017-9-17  Shi Ruitao
  */
 
-var ag = 0; 
+var ag = 1; 
 var func = function(){
-    ag++;
     var obj = {
-        age: ag
+        age: ag++
     }
     return obj;
 }
 var a = func();
-console.log(a.age);
 var b = func();
-console.log(b.age);
 var c = func();
-console.log(c.age);
 var d = func();
-console.log(d.age);
+console.log(a, b, c, d);
