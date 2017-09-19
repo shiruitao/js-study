b8b107bb52dcccba6143d2158b42b4340cf6a4e6
shitime <1257201113@qq.com>
2017-09-19 20:44:12 +0800

    Function中apply bind call的用法

--- bind.js
+++ bind.js
@@ -12,12 +12,14 @@ var a = function(x, y){
 console.log(a.apply(b, [2,3]), b);
 
 console.log('-----------bind------------')
-this.age1 = 2;
-var obj = {
-    age1: 1,
-    fun: function(p1, p2, p3){
-        return this.age1 + p1 + p2 + p3;
-    }
+var c = {
+    name: 'abc',
 };
-var f = obj.fun;
-console.log(f.bind(obj, 1, 2, 3)());
+var d = arrayToDestruct = function(x, y){
+    this.age = 3;
+    return this.age+ x + y;
+};
+console.log(a.bind(b, 1, 2)(), b)
+
+console.log('-----------call------------')
+console.log(a.call(b, 2, 2), b)
